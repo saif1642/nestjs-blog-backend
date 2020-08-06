e67dced17be123f5c5c76d841b16c2aa130675a8
saifdev1642 <saifdev1642@gmail.com>
2020-08-06 06:00:30 +0600

    database connenction setup

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,13 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { DbConnectionService } from './db-connection.service';
 
 @Module({
-  imports: [],
+  imports: [TypeOrmModule.forRootAsync({
+    useClass:DbConnectionService
+  })],
   controllers: [AppController],
   providers: [AppService],
 })
